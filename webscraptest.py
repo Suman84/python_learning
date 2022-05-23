92cb2f245ae71df4840919a5c61cd6b37cb6ef33
suman <sumankhanal770@gmail.com>
2022-05-23 23:45:22 +0545

    steam 1st item name and price community market

--- webscraptest.py
+++ webscraptest.py
@@ -5,5 +5,6 @@ html_text = requests.get('https://steamcommunity.com/market/').text
 soup = BeautifulSoup(html_text, 'html.parser')
 item = soup.find('div', class_="market_listing_row market_recent_listing_row market_listing_searchresult")
 item_name = item.find('span', class_ = 'market_listing_item_name').text.replace(" ","_")
-print(item_name)
+item_price = item.find('span', class_ = 'sale_price').text
+print(item_name +' = '+ item_price)
 #print(html_text)
