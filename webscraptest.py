52e074987edead93cab9c0ae3440a475b7dce38f
suman <sumankhanal770@gmail.com>
2022-05-24 11:54:56 +0545

    multiple items

--- webscraptest.py
+++ webscraptest.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://steamcommunity.com/market/').text
+html_text = requests.get('https://steamcommunity.com/market').text
 soup = BeautifulSoup(html_text, 'html.parser')
-item = soup.find('div', class_="market_listing_row market_recent_listing_row market_listing_searchresult")
-item_name = item.find('span', class_ = 'market_listing_item_name').text.replace(" ","_")
-item_price = item.find('span', class_ = 'sale_price').text
-print(item_name +' = '+ item_price)
-#print(html_text)
+items = soup.find_all('div', class_="market_listing_row market_recent_listing_row market_listing_searchresult")
+for item in items:
+    item_name = item.find('span', class_='market_listing_item_name').text.replace(" ", "_")
+    item_price = item.find('span', class_='sale_price').text
+    print(item_name + ' = ' + item_price)
